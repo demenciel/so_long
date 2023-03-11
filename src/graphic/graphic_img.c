85e41dbd7215c496d31f3a08c660fb1ef9786585
Alexandre Couture <acouture@c1r5p1.42quebec.com>
2023-03-11 10:13:12 -0500

    norminette done

--- src/graphic/graphic_img.c
+++ src/graphic/graphic_img.c
@@ -6,7 +6,7 @@
 /*   By: acouture <acouture@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 14:40:05 by acouture          #+#    #+#             */
-/*   Updated: 2023/03/11 10:05:17 by acouture         ###   ########.fr       */
+/*   Updated: 2023/03/11 10:09:37 by acouture         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,65 +15,65 @@
 void	textures_to_img(t_data *data)
 {
 	data->img_t.tiles_img = mlx_texture_to_image(data->mlx,
-													data->textures.tiles);
+			data->textures.tiles);
 	data->img_t.walls_img = mlx_texture_to_image(data->mlx,
-													data->textures.walls);
+			data->textures.walls);
 	data->img_t.collec_img = mlx_texture_to_image(data->mlx,
-													data->textures.collectible);
+			data->textures.collectible);
 	data->img_t.exit_img = mlx_texture_to_image(data->mlx,
-												data->textures.exit);
+			data->textures.exit);
 	data->img_t.wood_img = mlx_texture_to_image(data->mlx,
-												data->textures.wood);
+			data->textures.wood);
 }
 
-// mlx_image_t	*load_player(t_data *data)
-// {
-// 	loop_player(data);
-// 	if (data->direction == 1)
-// 	{
-// 		data->img_t.player = mlx_texture_to_image(data->mlx,
-// 													data->textures.player_front[data->anim.p]);
-// 	}
-// 	if (data->direction == 0)
-// 	{
-// 		data->img_t.player = mlx_texture_to_image(data->mlx,
-// 													data->textures.player_back[data->anim.p]);
-// 	}
-// 	if (data->direction == 2)
-// 	{
-// 		data->img_t.player = mlx_texture_to_image(data->mlx,
-// 													data->textures.player_left[data->anim.p]);
-// 	}
-// 	if (data->direction == 3)
-// 	{
-// 		data->img_t.player = mlx_texture_to_image(data->mlx,
-// 													data->textures.player_right[data->anim.p]);
-// 	}
-// 	return (data->img_t.player);
-// }
+mlx_image_t	*load_player(t_data *data)
+{
+	loop_player(data);
+	if (data->direction == 1)
+	{
+		data->img_t.player = mlx_texture_to_image(data->mlx,
+				data->textures.player_front[data->anim.p]);
+	}
+	if (data->direction == 0)
+	{
+		data->img_t.player = mlx_texture_to_image(data->mlx,
+				data->textures.player_back[data->anim.p]);
+	}
+	if (data->direction == 2)
+	{
+		data->img_t.player = mlx_texture_to_image(data->mlx,
+				data->textures.player_left[data->anim.p]);
+	}
+	if (data->direction == 3)
+	{
+		data->img_t.player = mlx_texture_to_image(data->mlx,
+				data->textures.player_right[data->anim.p]);
+	}
+	return (data->img_t.player);
+}
 
-// mlx_image_t	*load_enemy(t_data *data)
-// {
-// 	loop_enemy(data);
-// 	data->img_t.enemy_img = mlx_texture_to_image(data->mlx,
-// 													data->textures.enemy[data->anim.e]);
-// 	return (data->img_t.enemy_img);
-// }
+mlx_image_t	*load_enemy(t_data *data)
+{
+	loop_enemy(data);
+	data->img_t.enemy_img = mlx_texture_to_image(data->mlx,
+			data->textures.enemy[data->anim.e]);
+	return (data->img_t.enemy_img);
+}
 
-// mlx_image_t	*load_images(char c, t_data *data)
-// {
-// 	if (c == '1')
-// 		return (data->img_t.walls_img);
-// 	if (c == '2')
-// 		return (data->img_t.wood_img);
-// 	if (c == 'P')
-// 		return (load_player(data));
-// 	if (c == 'C')
-// 		return (data->img_t.collec_img);
-// 	if (c == 'A')
-// 		return (load_enemy(data));
-// 	if (c == 'E')
-// 		return (data->img_t.exit_img);
-// 	else
-// 		return (data->img_t.tiles_img);
-// }
+mlx_image_t	*load_images(char c, t_data *data)
+{
+	if (c == '1')
+		return (data->img_t.walls_img);
+	if (c == '2')
+		return (data->img_t.wood_img);
+	if (c == 'P')
+		return (load_player(data));
+	if (c == 'C')
+		return (data->img_t.collec_img);
+	if (c == 'A')
+		return (load_enemy(data));
+	if (c == 'E')
+		return (data->img_t.exit_img);
+	else
+		return (data->img_t.tiles_img);
+}
--- src/graphic/graphic_move.c
+++ src/graphic/graphic_move.c
@@ -6,108 +6,108 @@
 /*   By: acouture <acouture@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 14:19:16 by acouture          #+#    #+#             */
-/*   Updated: 2023/03/10 15:57:22 by acouture         ###   ########.fr       */
+/*   Updated: 2023/03/11 10:10:56 by acouture         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/so_long.h"
 
-void    ft_up(t_data *data)
+void	ft_up(t_data *data)
 {
-    int x;
-    int y;
+	int	x;
+	int	y;
 
-    x = data->player.x;
-    y = data->player.y;
-    data->direction = 0;
-    check_exit(x, y - 1, data);
-    if (data->map_cpy[y - 1][x] == '0' || data->map_cpy[y - 1][x] == 'C')
-    {
-        if (data->map_cpy[y - 1][x] == 'C')
-            data->elem.collec--;
-        data->map_cpy[y - 1][x] = 'P';
-        data->player.y -= 1;
-        data->map_cpy[y][x] = '0';
-        data->count_moves++;
-    }
+	x = data->player.x;
+	y = data->player.y;
+	data->direction = 0;
+	check_exit(x, y - 1, data);
+	if (data->map_cpy[y - 1][x] == '0' || data->map_cpy[y - 1][x] == 'C')
+	{
+		if (data->map_cpy[y - 1][x] == 'C')
+			data->elem.collec--;
+		data->map_cpy[y - 1][x] = 'P';
+		data->player.y -= 1;
+		data->map_cpy[y][x] = '0';
+		data->count_moves++;
+	}
 }
 
-void    ft_down(t_data *data)
+void	ft_down(t_data *data)
 {
-    int x;
-    int y;
+	int	x;
+	int	y;
 
-    x = data->player.x;
-    y = data->player.y;
-    data->direction = 1;
-    check_exit(x, y + 1, data);
-    if (data->map_cpy[y + 1][x] == '0' || data->map_cpy[y + 1][x] == 'C')
-    {
-        if (data->map_cpy[y + 1][x] == 'C')
-            data->elem.collec--;
-        data->map_cpy[y + 1][x] = 'P';
-        data->player.y += 1;
-        data->map_cpy[y][x] = '0';
-        data->count_moves++;
-    }
+	x = data->player.x;
+	y = data->player.y;
+	data->direction = 1;
+	check_exit(x, y + 1, data);
+	if (data->map_cpy[y + 1][x] == '0' || data->map_cpy[y + 1][x] == 'C')
+	{
+		if (data->map_cpy[y + 1][x] == 'C')
+			data->elem.collec--;
+		data->map_cpy[y + 1][x] = 'P';
+		data->player.y += 1;
+		data->map_cpy[y][x] = '0';
+		data->count_moves++;
+	}
 }
 
-void    ft_left(t_data *data)
+void	ft_left(t_data *data)
 {
-    int x;
-    int y;
+	int	x;
+	int	y;
 
-    x = data->player.x;
-    y = data->player.y;
-    data->direction = 2;
-    check_exit(x - 1, y, data);
-    if (data->map_cpy[y][x - 1] == '0' || data->map_cpy[y][x - 1] == 'C')
-    {
-        if (data->map_cpy[y][x - 1] == 'C')
-            data->elem.collec--;
-        data->map_cpy[y][x - 1] = 'P';
-        data->player.x -= 1;
-        data->map_cpy[y][x] = '0';
-        data->count_moves++;
-    }
+	x = data->player.x;
+	y = data->player.y;
+	data->direction = 2;
+	check_exit(x - 1, y, data);
+	if (data->map_cpy[y][x - 1] == '0' || data->map_cpy[y][x - 1] == 'C')
+	{
+		if (data->map_cpy[y][x - 1] == 'C')
+			data->elem.collec--;
+		data->map_cpy[y][x - 1] = 'P';
+		data->player.x -= 1;
+		data->map_cpy[y][x] = '0';
+		data->count_moves++;
+	}
 }
 
-void    ft_right(t_data *data)
+void	ft_right(t_data *data)
 {
-    int x;
-    int y;
+	int	x;
+	int	y;
 
-    x = data->player.x;
-    y = data->player.y;
-    data->direction = 3;
-    check_exit(x + 1, y, data);
-    if (data->map_cpy[y][x + 1] == '0' || data->map_cpy[y][x + 1] == 'C')
-    {
-        if (data->map_cpy[y][x + 1] == 'C')
-            data->elem.collec--;
-        data->map_cpy[y][x + 1] = 'P';
-        data->player.x += 1;
-        data->map_cpy[y][x] = '0';
-        data->count_moves++;
-    }
+	x = data->player.x;
+	y = data->player.y;
+	data->direction = 3;
+	check_exit(x + 1, y, data);
+	if (data->map_cpy[y][x + 1] == '0' || data->map_cpy[y][x + 1] == 'C')
+	{
+		if (data->map_cpy[y][x + 1] == 'C')
+			data->elem.collec--;
+		data->map_cpy[y][x + 1] = 'P';
+		data->player.x += 1;
+		data->map_cpy[y][x] = '0';
+		data->count_moves++;
+	}
 }
 
-void    my_keyhook(mlx_key_data_t keydata, void *param)
+void	my_keyhook(mlx_key_data_t keydata, void *param)
 {
-    t_data *data;
+	t_data	*data;
 
-    data = param;
-    if (keydata.action == MLX_PRESS || keydata.action == MLX_REPEAT)
-    {
-        if (keydata.key == MLX_KEY_W || keydata.key == MLX_KEY_UP)
-            ft_up(data);
-        if (keydata.key == MLX_KEY_S || keydata.key == MLX_KEY_DOWN)
-            ft_down(data);
-        if (keydata.key == MLX_KEY_A || keydata.key == MLX_KEY_LEFT)
-            ft_left(data);
-        if (keydata.key == MLX_KEY_D || keydata.key == MLX_KEY_RIGHT)
-            ft_right(data);
-        if (keydata.key == MLX_KEY_ESCAPE)
-            exit(0);
-    }
+	data = param;
+	if (keydata.action == MLX_PRESS || keydata.action == MLX_REPEAT)
+	{
+		if (keydata.key == MLX_KEY_W || keydata.key == MLX_KEY_UP)
+			ft_up(data);
+		if (keydata.key == MLX_KEY_S || keydata.key == MLX_KEY_DOWN)
+			ft_down(data);
+		if (keydata.key == MLX_KEY_A || keydata.key == MLX_KEY_LEFT)
+			ft_left(data);
+		if (keydata.key == MLX_KEY_D || keydata.key == MLX_KEY_RIGHT)
+			ft_right(data);
+		if (keydata.key == MLX_KEY_ESCAPE)
+			exit(0);
+	}
 }
